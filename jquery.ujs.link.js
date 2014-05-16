128d409931fa3906d80578e527f2591b301e6260
Regis leray <regis.leray@gmail.com>
2014-05-16 18:25:11 -0400

    live method deprecated
    change live(event) method which is deprecated by on(event) method

--- jquery.ujs.link.js
+++ jquery.ujs.link.js
@@ -24,14 +24,14 @@ https://github.com/rails/jquery-ujs/issues/168
     }
 
     //add the dynamic parameters in the form
-    $('a[data-with], a[data-submit]').live('ajax:before', function(event) {
+    $('a[data-with], a[data-submit]').on('ajax:before', function(event) {
         var el = $(this);
         //data-params permits to send custom data in the ajax request
         el.attr('data-params', retreiveData(el));
     });
 
     //clean the attribute, dont need anymore
-    $('a[data-with], a[data-submit]').live('ajax:complete', function(event, xhr, status) {
+    $('a[data-with], a[data-submit]').on('ajax:complete', function(event, xhr, status) {
         var el = $(this);
         el.removeAttr('data-params');
         el.removeData('params');
